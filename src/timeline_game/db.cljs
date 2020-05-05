5db3e2662323a6cc9a11fab76616fdecd6f123e8
regul <regul@mantis>
2020-05-05 16:35:06 +0200

    feat: auto-generate card ids

--- src/timeline_game/db.cljs
+++ src/timeline_game/db.cljs
@@ -1,23 +1,15 @@
 (ns timeline-game.db)
 
-(def cards [{:id 0
-             :title "Adam"}
-            {:id 1
-             :title "Noe"}
-            {:id 2
-             :title "Abraham"}
-            {:id 3
-             :title "Izák"}
-            {:id 4
-             :title "Jákob"}
-            {:id 5
-             :title "David"}
-            {:id 6
-             :title "Šalomoun"}
-            {:id 7
-             :title "Eliáš"}
-            {:id 8
-             :title "Daniel"}])
+(def cards [{:title "Adam"}
+            {:title "Noe"}
+            {:title "Abraham"}
+            {:title "Izák"}
+            {:title "Jákob"}
+            {:title "David"}
+            {:title "Šalomoun"}
+            {:title "Eliáš"}
+            {:title "Daniel"}])
 
 (def default-db
-  {:cards (reduce #(assoc %1 (:id %2) %2) {} cards)})
+  (let [cards-with-id (map #(assoc %1 :id %2) cards (range))]
+    {:cards (reduce #(assoc %1 (:id %2) %2) {} cards-with-id)}))
