b34b9721bdca58b05484e3e9cd701d99ab56a8a3
regul <regul@mantis>
2020-05-14 23:22:37 +0200

    feat: expand card set

--- src/timeline_game/db.cljs
+++ src/timeline_game/db.cljs
@@ -1,14 +1,41 @@
 (ns timeline-game.db)
 
 (def cards [{:title "Adam"}
+            {:title "Enoch"}
             {:title "Noe"}
+            {:title "Nimrod"}
             {:title "Abraham"}
             {:title "Izák"}
             {:title "Jákob"}
+            {:title "Dina"}
+            {:title "Josef"}
+            {:title "Mojžíš"}
+            {:title "Jozue"}
+            {:title "Balám"}
+            {:title "Raab"}
+            {:title "Debora"}
+            {:title "Rut"}
+            {:title "Gideon"}
+            {:title "Jefta"}
+            {:title "Samuel"}
+            {:title "Saul"}
             {:title "David"}
+            {:title "Abigail"}
             {:title "Šalomoun"}
+            {:title "Jezábel"}
+            {:title "Jošafat"}
             {:title "Eliáš"}
-            {:title "Daniel"}])
+            {:title "Eliša"}
+            {:title "Jonáš"}
+            {:title "Ezekjáš"}
+            {:title "Josijáš"}
+            {:title "Jeremjáš"}
+            {:title "Daniel"}
+            {:title "Ester"}
+            {:title "Nehemjáš"}
+            {:title "Jan Křtitel"}
+            {:title "Marie"}
+            {:title "Ježíš"}])
 
 (def default-db
   (let [cards-with-id (map #(assoc %1 :id %2) cards (range))]
