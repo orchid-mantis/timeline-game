28481a6abb96dce89b070174248d8068fb0a942b
regul <regul@mantis>
2020-05-18 21:57:33 +0200

    feat: introduce bot player

--- src/timeline_game/core.cljs
+++ src/timeline_game/core.cljs
@@ -7,22 +7,30 @@
 
 (def hand-size 5)
 
+(def players [:player :bot])
+
 (defn init-deck [db]
   (let [cards (keys (:cards db))
         deck (shuffle cards)]
     (assoc db :deck deck)))
 
-(defn init-hand [db]
-  (let [cards-ids (take hand-size (:deck db))]
-    (-> db
-        (assoc-in [:player :hand] cards-ids)
-        (update :deck #(drop hand-size %)))))
+(defn deal-cards [hand-size deck]
+  [(take hand-size deck) (drop hand-size deck)])
 
-(defn init-player [db]
+(defn init-hand [db player]
+  (let [[hand deck] (deal-cards hand-size (:deck db))]
+    (-> db
+        (assoc-in [player :hand] hand)
+        (assoc :deck deck))))
+
+(defn init-player [db player]
   (-> db
-      (assoc :player {:active? true})
-      (assoc-in [:player :history] {:ids '() :validity {}})
-      init-hand))
+      (assoc player {:active? true})
+      (assoc-in [player :history] {:ids '() :validity {}})
+      (init-hand player)))
+
+(defn init-players [db players]
+  (reduce #(init-player %1 %2) db players))
 
 (defn init-timeline [db]
   (let [cards-ids (take 1 (:deck db))]
@@ -38,7 +46,7 @@
       init-game-state
       init-deck
       init-timeline
-      init-player))
+      (init-players players)))
 
 (rf/reg-event-db
  :new-game
