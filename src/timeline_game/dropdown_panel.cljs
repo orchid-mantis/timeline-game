de4197226b38440d7082b5adc888db8b19af6597
regul <regul@mantis>
2020-05-22 23:02:06 +0200

    ref: move component styles to main.css

--- src/timeline_game/dropdown_panel.cljs
+++ src/timeline_game/dropdown_panel.cljs
@@ -7,19 +7,11 @@
     (fn [title header content]
       (let [open? (:open @s)]
         [:div.dropdown {:style {:float :left}}
-         [:div.dropbtn {:on-click #(swap! s update :open not)
-                        :style {:margin "5px"
-                                :padding "10px"}}
-
-          [:div {:style {:width "30px"
-                         :height "30px"
-                         :display :inline-block
-                         :margin-right "10px"
-                         :float :left}}
+         [:div.dropbtn {:on-click #(swap! s update :open not)}
+          [:div {:style {:float :left}}
            [:i.arrow {:class (if open? :down :right)}]]
           [:span title]
           header]
 
-         [:div.dropdown-content {:style {:display (if open? :block :none)
-                                         :left 0}}
+         [:div.dropdown-content {:style {:display (if open? :block :none)}}
           content]]))))
