61bc4ea5b76bd74e1bacb1d52660a35b5436c23b
regul <regul@mantis>
2020-05-26 21:01:00 +0200

    ref: move bot player logic to separate file

---
+++ src/timeline_game/bot_player.cljs
@@ -0,0 +1,41 @@
+(ns timeline-game.bot-player
+  (:require [re-frame.core :as rf]
+            [clojure.set :as set]
+            [timeline-game.game-loop]
+            [timeline-game.common :refer [put-before remove-card]]))
+
+(defn select-card [hand]
+  (let [card-id (first (shuffle hand))]
+    [card-id (remove-card hand card-id)]))
+
+(defn place-card-correct [timeline card-id]
+  (let [[lesser greater] (split-with #(> card-id %) timeline)]
+    (vec (concat lesser [card-id] greater))))
+
+(defn find-index [needle haystack]
+  (first (keep-indexed #(when (= %2 needle) %1) haystack)))
+
+(defn place-card-wrong [timeline card-id]
+  (let [correct-timeline (place-card-correct timeline card-id)
+        correct-pos (find-index card-id correct-timeline)
+        all-pos (range (count correct-timeline))
+        all-wrong-pos (into [] (set/difference (set all-pos) #{correct-pos}))
+        random-pos (rand-nth all-wrong-pos)]
+    (put-before timeline random-pos card-id)))
+
+(defn bot-place-card [timeline card-id bot-success?]
+  (cond
+    (nil? card-id) timeline
+    bot-success? (place-card-correct timeline card-id)
+    :else (place-card-wrong timeline card-id)))
+
+(rf/reg-event-fx
+ :play-bot-move
+ (fn [{:keys [db]} _]
+   (let [[id new-hand] (select-card (get-in db [:bot :hand]))
+         distribution (get-in db [:bot :success-dist])]
+     {:db (-> db
+              (assoc-in [:bot :hand] new-hand)
+              (update-in [:timeline :ids] bot-place-card id (peek distribution))
+              (assoc-in [:bot :success-dist] (pop distribution)))
+      :dispatch [:eval-move :bot id]})))
--- src/timeline_game/events.cljs
+++ src/timeline_game/events.cljs
@@ -1,7 +1,7 @@
 (ns timeline-game.events
   (:require [re-frame.core :as rf]
-            [clojure.set :as set]
             [timeline-game.game-loop]
+            [timeline-game.bot-player]
             [timeline-game.common :refer [put-before remove-card ordered?]]
             [timeline-game.fsm :as fsm]))
 
@@ -132,42 +132,6 @@
             (update-next-state event))
     :dispatch [:end-turn]}))
 
-(defn select-card [hand]
-  (let [card-id (first (shuffle hand))]
-    [card-id (remove-card hand card-id)]))
-
-(defn place-card-correct [timeline card-id]
-  (let [[lesser greater] (split-with #(> card-id %) timeline)]
-    (vec (concat lesser [card-id] greater))))
-
-(defn find-index [needle haystack]
-  (first (keep-indexed #(when (= %2 needle) %1) haystack)))
-
-(defn place-card-wrong [timeline card-id]
-  (let [correct-timeline (place-card-correct timeline card-id)
-        correct-pos (find-index card-id correct-timeline)
-        all-pos (range (count correct-timeline))
-        all-wrong-pos (into [] (set/difference (set all-pos) #{correct-pos}))
-        random-pos (rand-nth all-wrong-pos)]
-    (put-before timeline random-pos card-id)))
-
-(defn bot-place-card [timeline card-id bot-success?]
-  (cond
-    (nil? card-id) timeline
-    bot-success? (place-card-correct timeline card-id)
-    :else (place-card-wrong timeline card-id)))
-
-(rf/reg-event-fx
- :play-bot-move
- (fn [{:keys [db]} _]
-   (let [[id new-hand] (select-card (get-in db [:bot :hand]))
-         distribution (get-in db [:bot :success-dist])]
-     {:db (-> db
-              (assoc-in [:bot :hand] new-hand)
-              (update-in [:timeline :ids] bot-place-card id (peek distribution))
-              (assoc-in [:bot :success-dist] (pop distribution)))
-      :dispatch [:eval-move :bot id]})))
-
 (defn evaluate-round [[player-hand bot-hand]]
   (cond
     (and (empty? player-hand) (empty? bot-hand)) [false :tie]
@@ -189,4 +153,3 @@
  :game-end
  (fn [db [_ game-result]]
    (assoc-in db [:game :result] game-result)))
-
