b5e16c099ccaac48ec4987dcf9bb79f3df1ecc75
regul <regul@mantis>
2020-06-01 22:59:32 +0200

    ref: prepare data inside subs

--- src/timeline_game/game_result.cljs
+++ src/timeline_game/game_result.cljs
@@ -6,38 +6,51 @@
  (fn [db]
    (get-in db [:game :result])))
 
-(defn view []
-  (let [game-result (rf/subscribe [:game-result])]
-    (fn []
-      (let [show? (not= @game-result :await)
-            display-data (case @game-result
-                           :player-won {:text "You won!" :color :green}
-                           :player-lost {:text "Game over" :color :red}
-                           nil)]
-        [:div {:style {:position :fixed
-                       :display (if show? :block :none)
-                       :width "100%"
-                       :height "100%"
-                       :top 0
-                       :left 0
-                       :right 0
-                       :bottom 0
-                       :background-color "rgba(255,255,255,0.9)"
-                       :z-index 2}}
-         [:div {:style {:position :absolute
-                        :top "40%"
-                        :left "50%"
-                        :transform "translate(-50%,-50%)"
-                        :-ms-transform "translate(-50%,-50%)"}}
-          [:h1 {:style {:font-size "50px"
-                        :color (:color display-data)}}
-           (:text display-data)]
+(rf/reg-sub
+ :game-result/show?
+ (fn []
+   (rf/subscribe [:game-result]))
+ (fn [result]
+   (not= result :await)))
 
-          [:button
-           {:on-click #(rf/dispatch [:new-game])
-            :style {:width 150
-                    :padding "10px 20px"
-                    :display :block
-                    :margin "10px auto 0px auto"
-                    :cursor :pointer}}
-           "Start a new game"]]]))))
+(rf/reg-sub
+ :game-result/view-data
+ (fn []
+   (rf/subscribe [:game-result]))
+ (fn [result]
+   (case result
+     :player-won {:text "You won!" :color :green}
+     :player-lost {:text "Game over" :color :red}
+     nil)))
+
+(defn view []
+  (let [show? (rf/subscribe [:game-result/show?])
+        view-data (rf/subscribe [:game-result/view-data])]
+    (fn []
+      [:div {:style {:position :fixed
+                     :display (if @show? :block :none)
+                     :width "100%"
+                     :height "100%"
+                     :top 0
+                     :left 0
+                     :right 0
+                     :bottom 0
+                     :background-color "rgba(255,255,255,0.9)"
+                     :z-index 2}}
+       [:div {:style {:position :absolute
+                      :top "40%"
+                      :left "50%"
+                      :transform "translate(-50%,-50%)"
+                      :-ms-transform "translate(-50%,-50%)"}}
+        [:h1 {:style {:font-size "50px"
+                      :color (:color @view-data)}}
+         (:text @view-data)]
+
+        [:button
+         {:on-click #(rf/dispatch [:new-game])
+          :style {:width 150
+                  :padding "10px 20px"
+                  :display :block
+                  :margin "10px auto 0px auto"
+                  :cursor :pointer}}
+         "Start a new game"]]])))
