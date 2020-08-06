cba329fb0e7fc262dc36c201d8515dc1481f69f4
regul <regul@mantis>
2020-08-06 23:31:02 +0200

    feat: simplify dropdown

--- src/timeline_game/dropdown_panel.cljs
+++ src/timeline_game/dropdown_panel.cljs
@@ -2,16 +2,14 @@
   (:require [reagent.core :as reagent]
             [re-frame.core :as rf]))
 
-(defn view [title header content]
+(defn view [title content]
   (let [s (reagent/atom {:open false})]
-    (fn [title header content]
+    (fn [title content]
       (let [open? (:open @s)]
-        [:div.dropdown {:style {:float :left}}
-         [:div.dropbtn {:on-click #(swap! s update :open not)}
-          [:div {:style {:float :left}}
-           [:i.arrow {:class (if open? :down :right)}]]
-          [:span title]
-          header]
+        [:div.dropdown
+         [:div.button {:on-click #(swap! s update :open not)}
+          [:i.arrow {:class (if open? :down :right)}]
+          [:span title]]
 
          [:div.dropdown-content {:style {:display (if open? :block :none)}}
           content]]))))
