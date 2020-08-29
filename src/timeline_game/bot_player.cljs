a3456b6a4986fec7d732b8bfbf896316a449c6e4
regul <regul@mantis>
2020-08-29 16:23:51 +0200

    feat: auto-scroll for bot player

--- src/timeline_game/bot_player.cljs
+++ src/timeline_game/bot_player.cljs
@@ -8,20 +8,20 @@
   (let [card-id (first (shuffle hand))]
     [card-id (remove-card hand card-id)]))
 
-(defn place-card-correct [timeline card-id]
-  (let [[lesser greater] (split-with #(> card-id %) timeline)]
-    (vec (concat lesser [card-id] greater))))
-
 (defn find-index [needle haystack]
   (first (keep-indexed #(when (= %2 needle) %1) haystack)))
 
+(defn place-card-correct [timeline card-id]
+  (let [[lesser greater] (split-with #(> card-id %) timeline)
+        new-timeline (vec (concat lesser [card-id] greater))]
+    [(find-index card-id new-timeline) new-timeline]))
+
 (defn place-card-wrong [timeline card-id]
-  (let [correct-timeline (place-card-correct timeline card-id)
-        correct-pos (find-index card-id correct-timeline)
+  (let [[correct-pos correct-timeline] (place-card-correct timeline card-id)
         all-pos (range (count correct-timeline))
         all-wrong-pos (into [] (set/difference (set all-pos) #{correct-pos}))
         random-pos (rand-nth all-wrong-pos)]
-    (put-before timeline random-pos card-id)))
+    [random-pos (put-before timeline random-pos card-id)]))
 
 (defn bot-place-card [timeline card-id bot-success?]
   (cond
@@ -38,8 +38,73 @@
  :play-bot-move
  [(rf/inject-cofx :bot-success?)]
  (fn [{:keys [db bot-success?]} _]
-   (let [[id new-hand] (select-card (get-in db [:bot :hand :ids]))]
+   (let [[id new-hand] (select-card (get-in db [:bot :hand :ids]))
+         timeline (get-in db [:timeline :ids])
+         [pos new-timeline] (bot-place-card timeline id bot-success?)
+         timeline-node (get-in db [:dom-nodes :timeline])]
+     (js/console.log (str "Bots card pos = " pos))
+     {:db (assoc-in db [:bot :last-move] {:id id
+                                          :hand new-hand
+                                          :timeline new-timeline})
+      :side-scroll [timeline-node pos #(rf/dispatch [:side-scroll-finished])]})))
+
+(defn side-scroll [el direction speed distance step callback]
+  (let [scroll-amount (atom 0)
+        op (case direction
+             :left  -
+             :right +)
+        timer (atom nil)]
+    (reset! timer (js/setInterval (fn [] (let [scroll-left (.-scrollLeft el)
+                                               next-scroll-amount (+ @scroll-amount step)
+                                               diff (if (> next-scroll-amount distance)
+                                                      (- next-scroll-amount distance)
+                                                      0)]
+                                           (swap! scroll-amount #(+ % step))
+                                           (set! (.-scrollLeft el) (op scroll-left (- step diff)))
+                                          ;;  (js/console.log scroll-left)
+                                           (when (>= @scroll-amount distance)
+                                             (js/clearInterval @timer)
+                                             (callback))))
+                                  speed))))
+(rf/reg-fx
+ :side-scroll
+ (fn [[node pos callback]]
+   (let [max-dist (- (.-scrollWidth node) (.-clientWidth node))
+         current-pos (.-scrollLeft node)
+         target-pos (->
+                        ;; (quot dist-to-pos client-width)
+                        ;; (* client-width)
+                     (* pos (+ 166 20))
+                     (#(if (> pos 0)
+                         (+ % 40)
+                         %))
+                     (min max-dist))
+         diff (- current-pos target-pos)
+         distance (if (< diff 0)
+                    (- diff)
+                    diff)]
+     (js/console.log (str "max-dist = " max-dist))
+     (js/console.log (str "clientWidth = " (.-clientWidth node)))
+     (js/console.log (str "target-pos = " target-pos " current-pos = " current-pos " distance = " distance))
+     (if (< target-pos current-pos)
+       (side-scroll node :left  25 distance 10 callback)
+       (side-scroll node :right 25 distance 10 callback)))))
+
+(rf/reg-event-fx
+ :side-scroll-finished
+ (fn [{:keys [db]} _]
+   (let [last-move (get-in db [:bot :last-move])]
      {:db (-> db
-              (assoc-in [:bot :hand :ids] new-hand)
-              (update-in [:timeline :ids] bot-place-card id bot-success?))
-      :dispatch [:eval-move :bot id]})))
+              (assoc-in [:bot :hand :ids] (:hand last-move))
+              (assoc-in [:timeline :ids] (:timeline last-move)))
+      :dispatch [:eval-move :bot (:id last-move)]})))
+
+
+;; (rf/reg-event-fx
+;;  :test-scroll
+;;  (fn [{:keys [db]} [_ pos]]
+;;    (let [timeline-node (get-in db [:dom-nodes :timeline])]
+;;      {:db db
+;;       :side-scroll [timeline-node pos #(do)]})))
+
+;; (rf/dispatch [:test-scroll 5])
