aed1da20a1b2ab148fe76de6311d4245bb61ae81
regul <regul@mantis>
2020-09-13 23:41:09 +0200

    feat: rework drag disabling

--- src/timeline_game/ui/hand/views.cljs
+++ src/timeline_game/ui/hand/views.cljs
@@ -10,50 +10,51 @@
    [timeline-game.ui.utils :as utils]))
 
 (defn draggable [card player-turn? drawn-card-id]
-  (let [pos (atom [0 0])
+  (let [s (atom {:pos [0 0]})
         id (:id card)]
     (reagent/create-class
      {:reagent-render
       (fn [card player-turn? drawn-card-id]
-        [:div.draggable
-         {;;  :draggable (when @player-turn? true)
-            ;;  :on-drag-start #(rf/dispatch [:select-card id])
-            ;;  :on-drag-end (fn []
-            ;;                 (rf/dispatch [:deselect-card]))
-          :style {:touch-action :none
-                  :user-select :none}}
-         [uic/basic-card-view
-          card
-          false
-          (utils/cs (when @player-turn? :selectable)
-                    (when (= id @drawn-card-id) :slide-in-top))
-          {:cursor (when (not @player-turn?) :not-allowed)
-           :opacity (when (not @player-turn?) 0.3)}]])
+        (let [node (:node @s)]
+          (when node
+            (.draggable (interact node) @player-turn?))
+          [:div.draggable
+           {:style {:touch-action :none
+                    :user-select :none}}
+           [uic/basic-card-view
+            card
+            false
+            (utils/cs (when @player-turn? :selectable)
+                      (when (= id @drawn-card-id) :slide-in-top))
+            {:cursor (when (not @player-turn?) :not-allowed)
+             :opacity (when (not @player-turn?) 0.3)}]]))
 
       :component-did-mount
       (fn [this]
-        (.draggable (interact (reagent-dom/dom-node this))
-                    #js {:inertia true
-                         :modifiers
-                         #js
-                          [(.restrictRect
-                            (.-modifiers interact)
-                            #js {:restriction ".table", :endOnly false})]
-                         :onmove (fn [e]
-                                   (let [[x y] @pos
-                                         new-x (+ (.-dx e) x)
-                                         new-y (+ (.-dy e) y)
-                                         target (.-target e)]
-                                     (reset! pos [new-x new-y])
-                                     (set!
-                                      (.. target -style -webkitTransform)
-                                      (set!
-                                       (.. target -style -transform)
-                                       (str "translate(" new-x "px, " new-y "px)")))))
+        (let [node (reagent-dom/dom-node this)]
+          (swap! s assoc :node node)
+          (.draggable (interact node)
+                      #js {:inertia true
+                           :modifiers
+                           #js
+                            [(.restrictRect
+                              (.-modifiers interact)
+                              #js {:restriction ".table", :endOnly false})]
+                           :onmove (fn [e]
+                                     (let [[x y] (:pos @s)
+                                           new-x (+ (.-dx e) x)
+                                           new-y (+ (.-dy e) y)
+                                           target (.-target e)]
+                                       (swap! s assoc :pos [new-x new-y])
+                                       (set!
+                                        (.. target -style -webkitTransform)
+                                        (set!
+                                         (.. target -style -transform)
+                                         (str "translate(" new-x "px, " new-y "px)")))))
 
-                         :onstart #(rf/dispatch [:select-card id])
+                           :onstart #(rf/dispatch [:select-card id])
 
-                         :onend   #(rf/dispatch [:deselect-card])}))})))
+                           :onend   #(rf/dispatch [:deselect-card])})))})))
 
 (defn view []
   (let [player-turn? (rf/subscribe [:players-turn?])
