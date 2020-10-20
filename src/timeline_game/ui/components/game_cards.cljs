9c8080c8c80792f860f257848aaca5f39ce3939e
regul <regul@mantis>
2020-10-20 22:00:46 +0200

    feat: add warped scroll text via cssWarp.js
    https://github.com/dirkweber/csswarp.js

--- src/timeline_game/ui/components/game_cards.cljs
+++ src/timeline_game/ui/components/game_cards.cljs
@@ -1,4 +1,7 @@
-(ns timeline-game.ui.components.game-cards)
+(ns timeline-game.ui.components.game-cards
+  (:require
+   [re-frame.core :as rf]
+   [reagent.core :as reagent]))
 
 (defn basic-card [card show-face? class style]
   (fn [card show-face? class style]
@@ -10,17 +13,54 @@
       (when show-face? [:h3.card__time-desc (:time-desc card)])]]))
 
 (defn image-card [card show-face? class style]
-  (let [href (if show-face? "#card-front" "#card-back")]
-    [:div.parent
-     {:class class
-      :style style}
+  (let [card-id (:id card)
+        scroll-text-id (str "text-" card-id)]
+    (reagent/create-class
+     {:reagent-render
+      (fn [card show-face? class style]
+        (let [href (if show-face? "#card-front" "#card-back")]
+          [:div.parent
+           {:class class
+            :style style}
 
-     [:svg.card-border
-      [:use
-       {:class (when show-face? :card-front)
-        :href href}]]
+           [:svg.card-border
+            [:use
+             {:class (when show-face? :card-front)
+              :href href}]]
 
-     [:div.card-border-clip
-      [:img.image {:src (str "images/cards/" (:img-name card) ".png")}]
-      [:div.content
-       [:h3.card-title (:title card)]]]]))
+           (when show-face?
+             [:div.scroll-text
+              {:id scroll-text-id}
+              (:time-desc card)])
+
+           [:div.card-border-clip
+            [:img.image {:src (str "images/cards/" (:img-name card) ".png")}]
+            [:div.content
+             [:h3.card-title (:title card)]]]]))
+
+      :component-did-mount
+      (fn []
+        (when show-face?
+          (rf/dispatch [:warp-scroll-text (str "#" scroll-text-id)])))})))
+
+(defn warp-text [targets]
+  (js/cssWarp (clj->js {:path [[35.2688 216.7838]
+                               [38.2112 217.7934 42.4588 220.438 50.7617 219.9646]
+                               [76.0438 218.5233 94.8823 196.9491 123.872 202.8383]]
+                        :targets targets
+                        :rotationMode "skew"
+                        :indent "1em"
+                        ;; :showPath {:color "red"
+                        ;;            :thickness 1}
+                        })))
+
+(rf/reg-event-fx
+ :warp-scroll-text
+ (fn [{:keys [db]} [_ targets]]
+   {:db db
+    :warp-text [targets]}))
+
+(rf/reg-fx
+ :warp-text
+ (fn [[targets]]
+   (warp-text targets)))
