4ca9ffd4a27d84be9ac36933023bf106eb182740
regul <regul@mantis>
2020-10-22 15:30:47 +0200

    feat: card-set - auto reload

--- src/timeline_game/ui/card_set/views.cljs
+++ src/timeline_game/ui/card_set/views.cljs
@@ -1,6 +1,7 @@
 (ns timeline-game.ui.card-set.views
   (:require
    [re-frame.core :as rf]
+   [reagent.core :as reagent]
    [timeline-game.ui.components :as ui]))
 
 (rf/reg-sub
@@ -11,9 +12,23 @@
    (sort-by :year <= (vals cards))))
 
 (defn view []
-  (let [cards (rf/subscribe [:card-set/ordered])]
-    [:div
-     (doall
-      (for [card @cards]
-        ^{:key (:id card)}
-        [ui/game-card card true]))]))
+  (let [timer (atom nil)
+        cards (rf/subscribe [:card-set/ordered])]
+    (reagent/create-class
+     {:reagent-render
+      (fn []
+        [:div
+         (doall
+          (for [card @cards]
+            ^{:key (:id card)}
+            [ui/game-card card true]))])
+
+      :component-did-mount
+      (fn []
+        (reset! timer (js/setInterval
+                       #(rf/dispatch [:cards/reload])
+                       2000)))
+
+      :component-will-unmount
+      (fn []
+        (js/clearInterval @timer))})))
