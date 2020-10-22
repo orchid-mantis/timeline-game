6bd966ee1df24fa753ca89eb87cecceb66248a15
regul <regul@mantis>
2020-10-22 19:28:00 +0200

    feat: adjust scroll-text positions

--- src/timeline_game/ui/components/game_cards.cljs
+++ src/timeline_game/ui/components/game_cards.cljs
@@ -11,21 +11,29 @@
     (when show-face?
       [:h3.card__time-desc (:time-desc card)])]])
 
-(defn warp-text [target indent]
+(defn warp-text [target {:keys [indent css]}]
   (js/cssWarp (clj->js {:path [[35.2688 216.7838]
                                [38.2112 217.7934 42.4588 220.438 50.7617 219.9646]
                                [76.0438 218.5233 94.8823 196.9491 123.872 202.8383]]
                         :targets target
                         :rotationMode "skew"
                         :indent indent
+                        :css css
                         ;; :showPath {:color "red"
                         ;;            :thickness 1}
                         })))
 
+(defn warp-text-options [{:keys [font-size indent top] :as opts}]
+  (if (map? opts)
+    {:indent indent
+     :css (str "font-size: " font-size "; top: " top ";")}
+    {:indent "0"
+     :css ""}))
+
 (defn image-card [card show-face? class]
   (let [card-id (:id card)
         scroll-text-id (str "text-" card-id)
-        indent (str (get card :scroll-text-indent 0) "%")]
+        options (warp-text-options (:scroll-text card))]
     (reagent/create-class
      {:reagent-render
       (fn [card show-face? class]
@@ -59,9 +67,9 @@
       :component-did-update
       (fn []
         (when show-face?
-          (warp-text (str "#" scroll-text-id) indent)))
+          (warp-text (str "#" scroll-text-id) options)))
 
       :component-did-mount
       (fn []
         (when show-face?
-          (warp-text (str "#" scroll-text-id) indent)))})))
+          (warp-text (str "#" scroll-text-id) options)))})))
