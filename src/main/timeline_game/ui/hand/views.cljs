d25a062ad1195640eace6714e40a03c7da02ad04
regul <regul@mantis>
2020-11-21 13:39:51 +0000

    feat: rotate & move cards - imitate cards in hand

--- src/main/timeline_game/ui/hand/views.cljs
+++ src/main/timeline_game/ui/hand/views.cljs
@@ -6,14 +6,55 @@
    [timeline-game.ui.hand.events]
    [timeline-game.ui.hand.subs]
    [timeline-game.ui.components :as ui]
-   [timeline-game.ui.utils :as utils]))
+   [timeline-game.ui.utils :as utils]
+   [dv.cljs-emotion-reagent :refer [defstyled]]))
 
-(defn draggable [game-card card allow-drag? drawn-card-id]
+(defn rotation [i o range]
+  (* range (/ (- o (/ (- i 1) 2))
+              (- i 2))))
+
+(defn offset [i o range]
+  (let [num (rotation i o range)]
+    (if (pos? num)
+      num
+      (- num))))
+
+(defn card-transform [max rotation-range offset-range]
+  (let [coll (for [card-count (range 1 (inc max))
+                   n (range 0 card-count)
+                   :let [rotation (rotation card-count n rotation-range)
+                         offset (offset card-count n offset-range)]]
+               [card-count [(inc n) [rotation offset]]])]
+    (reduce (fn [m [k1 [k2 v]]] (assoc-in m [k1 k2] v)) {} coll)))
+
+(def card-transform-precalc (card-transform 10 20 50))
+
+(defn ->keyword [value]
+  (keyword (str value)))
+
+(defstyled card :div
+  (fn [{:keys [card-transform total-cards n dragged?]}]
+    (let [[rotation offset] (get-in card-transform [(->keyword total-cards) (->keyword n)])]
+      {:transform (when (not dragged?) (str "translateY(" offset "px) rotate(" rotation "deg)"))
+       :backface-visibility :hidden
+       :margin "0 -10px"
+       ":hover" {:transform "translateY(-50px) rotate(0deg)"
+                 :transition-duration "450ms"
+                 :z-index 100}})))
+
+(defn card-in-hand [{:keys [index] :as options} el]
+  (card
+   (merge options
+          {:card-transform card-transform-precalc
+           :n (inc index)})
+   el))
+
+(defn draggable [game-card card index total-cards allow-drag? drawn-card-id]
   (let [s (reagent/atom {:pos [0 0] :z-index 0 :allow-drag? @allow-drag?})
         id (:id card)]
     (reagent/create-class
      {:reagent-render
-      (fn [game-card card allow-drag? drawn-card-id]
+      (fn [game-card card index total-cards allow-drag? drawn-card-id]
         (let [z-index (:z-index @s)
               [x y] (:pos @s)
               dragged? (:dragged? @s)]
@@ -22,16 +63,20 @@
             :style {:touch-action :none
                     :user-select :none
                     :position :relative
-                    :z-index z-index
+                    ;; :z-index z-index
                     :transform (str "translate(" x "px, " y "px)")
                     :transition (when (not dragged?) "0.5s")}}
-           [game-card
-            card
-            false
-            {:class (utils/cs (when @allow-drag? :selectable)
-                              (when (= id @drawn-card-id) :slide-in-top))
-             :style {:cursor (when (not @allow-drag?) :not-allowed)
-                     :opacity (when (not @allow-drag?) 0.3)}}]]))
+           (card-in-hand
+            {:total-cards total-cards
+             :index index
+             :dragged? dragged?}
+            [game-card
+             card
+             false
+             {:class (utils/cs (when @allow-drag? :selectable)
+                               (when (= id @drawn-card-id) :slide-in-top))
+              :style {:cursor (when (not @allow-drag?) :not-allowed)
+                      :opacity (when (not @allow-drag?) 0.3)}}])]))
 
       :component-did-update
       (fn [this]
@@ -71,9 +116,11 @@
         cards (rf/subscribe [:hand/cards])
         drawn-card-id (rf/subscribe [:drawn-card-id])]
     (fn []
-      [:div.hand
-       [:div.cards
-        (doall
-         (for [card @cards]
-           ^{:key (:id card)}
-           [draggable @game-card card allow-drag? drawn-card-id]))]])))
+      (let [total-cards (count @cards)]
+        [:div.hand
+         (doall
+          (map-indexed
+           (fn [index card]
+             ^{:key (:id card)}
+             [draggable @game-card card index total-cards allow-drag? drawn-card-id])
+           @cards))]))))
