936912ae66004ef259996589f0395520b7376ca1
regul <regul@mantis>
2021-01-02 22:20:04 +0000

    feat: fluent return from hover

--- src/main/timeline_game/ui/hand/views.cljs
+++ src/main/timeline_game/ui/hand/views.cljs
@@ -40,7 +40,8 @@
        :margin "0 -10px"
        ":hover" {:transform "translateY(-50px) rotate(0deg)"
                  :transition-duration "450ms"
-                 :z-index 1}})))
+                 :z-index 1}
+       :transition "800ms cubic-bezier(0.19, 1, 0.22, 1) transform"})))
 
 (defn card-in-hand [{:keys [index] :as options} el]
   (card
